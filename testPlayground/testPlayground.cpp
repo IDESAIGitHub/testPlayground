81676025938d6ec6648ec0e4e6fe233c8cacfa92
Ricardo <Ricardo@DESKTOP-NABODVE>
2023-01-02 18:10:39 +0100

    02/01/2023

--- testPlayground/testPlayground.cpp
+++ testPlayground/testPlayground.cpp
@@ -13,27 +13,27 @@
 #include <chrono>
 #include <string>
 
-std::string get_current_time_and_date()
-{
-    auto const time = std::chrono::current_zone()
-        ->to_local(std::chrono::system_clock::now());
-    return std::format("{:%Y-%m-%d %X}", time);
-}
+using namespace std::chrono;
 
 int main()
 {
 	std::cout << "Hello World!\n";
     cv::VideoCapture vcap;
+    vcap.set(cv::CAP_PROP_BUFFERSIZE, 1); // internal buffer will now store only 3 frames
     //mi movil sin pw(funciona)
     //const std::string videoStreamAddress = "http://192.168.0.27:8080/video";
     
     //mi movil con pw(funciona)
-    const std::string videoStreamAddress = "http://rchone:1234@192.168.0.27:8080/video";
+    const std::string videoStreamAddress = "http://rchone:1234@192.168.0.16:8080/video";
     
+    //pruebas
     //const std::string videoStreamAddress = "http://192.168.0.50:18320/video?x.mjpeg";
     // NNNhttp://USER:PWD@IPADDRESS:8088/mjpeg.cgi?user=USERNAME&password=PWD&channel=0&.mjpg
     
-    //const std::string videoStreamAddress = "http://admin:admin@192.168.0.50:8080/video";
+    //pruebas
+    //const std::string videoStreamAddress = "rtsp://admin:admin@192.168.0.21:554/1";
+    
+    
     
     //open the video stream and make sure it's opened
     if (!vcap.open(videoStreamAddress)) {
@@ -46,58 +46,103 @@ int main()
 	using namespace std::chrono;
 	seconds sec(0);
     int num_img = 0;
-    bool save_flag = true;
+    
     while (true)
     {
-		
+        auto start = high_resolution_clock::now();
 		bool s = vcap.read(frame);
         if (!s) {
             std::cout << "Can't read";
             break;
         }
         cv::imshow("First OpenCV Application", frame);
-        sec = duration_cast<seconds>(system_clock::now().time_since_epoch());
-		if (sec % seconds(10) == seconds(0)) {
-            if (save_flag)
-            {
-				save_flag = false;
-                std::string save_str = "";
-                num_img++;
-                save_str += "SaveImages//safuu" + std::to_string(num_img) + ".jpg";
-                cv::imwrite(save_str, frame);
-            }
-        }
-        else {
-            save_flag = true;
-        }
         
-        if (cv::waitKey(30) == 27) {
+        if (cv::waitKey(10) == 's') {
+            std::string save_str = "";
+            num_img++;
+            save_str += "SaveImages//safuu" + std::to_string(num_img) + ".jpg";
+            
+            cv::imwrite(save_str, frame);
+            
+            auto stop = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(stop - start);
+			std::cout << "Time taken by function: "
+				<< duration.count() << " microseconds" << std::endl;
             std::cout << "Bye bitch!\n";
             break;
         }
+
     }
     cv::destroyAllWindows();
+    
+    
+    
+    /*while (true)
+    {
+        auto start = high_resolution_clock::now();
+        bool s = vcap.read(frame);
+        if (!s) {
+            std::cout << "Can't read";
+            break;
+        }
+        if (cv::waitKey(20) == 's') {
+            std::string save_str = "";
+            num_img++;
+            save_str += "SaveImages//safuu" + std::to_string(num_img) + ".jpg";
+
+            cv::imwrite(save_str, frame);
+
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            std::cout << "Time taken by function: "
+                << duration.count() << " microseconds" << std::endl;
+            std::cout << "Bye bitch!\n";
+            break;
+        }
+    }*/
+    
+    
     return 0;
     
-    /*
-    //play Black Adam Trailer
-    using namespace cv;
-    Mat frame;
-    VideoCapture cap = VideoCapture("Images//Rock.mp4");
-    if (!cap.isOpened()) {
-        std::cout << "Error opening video stream or file\n";
-    }
-    bool finish = true;
-    while (finish)
-    {
-        finish = cap.read(frame);
-        cv::imshow("guapisimos", frame);
-        if (cv::waitKey(30) == 27) {
-            std::cout << "Bye bitch!\n";
-            finish = false;
-        }
-    }
-    cv::destroyAllWindows();
-    */
+    //cv::Mat img;
+    //const std::string imgPath = "Images/rock.jpg";
+    //img = cv::imread(imgPath, cv::IMREAD_COLOR);
+    //
+    //if (img.empty())
+    //{
+    //    std::cout << "Could not read the image: " << std::endl;
+    //    return 1;
+    //}
+    //imshow("Display window", img);
+    //int k = cv::waitKey(0); // Wait for a keystroke in the window
+    //if (k == 's')
+    //{
+    //    imwrite("starry_night.png", img);
+    //}
+    
+    
+    ////play Black Adam Trailer
+    //using namespace cv;
+    //Mat frame;
+    //VideoCapture cap = VideoCapture("Images//Rock.mp4");
+    //if (!cap.isOpened()) {
+    //    std::cout << "Error opening video stream or file\n";
+    //}
+    //bool finish = true;
+    //while (finish)
+    //{
+    //    finish = cap.read(frame);
+    //    cv::imshow("guapisimos", frame);
+    //    if (cv::waitKey(30) == 27) {
+    //        std::cout << "Bye bitch!\n";
+    //        finish = false;
+    //    }
+    //}
+    //cv::destroyAllWindows();
+    
+
+    //copilot do something!!
+    
+    
     return 0;
 }
